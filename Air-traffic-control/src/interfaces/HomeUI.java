d1464b7aa294d4094acc5fc987c27611e10f338b
digo-smithh <smithrodrigues08@gmail.com>
2020-06-08 16:16:40 -0300

    Interfaces

---
+++ Air-traffic-control/src/interfaces/HomeUI.java
@@ -0,0 +1,204 @@
+package interfaces;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+public class HomeUI extends javax.swing.JFrame 
+{
+    public HomeUI() 
+    {
+        initComponents();
+        this.setLocationRelativeTo(null);
+        this.setIconImage(new ImageIcon(getClass().getResource("/imagens/house2.png")).getImage());
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jMenuBar2 = new javax.swing.JMenuBar();
+        jMenu6 = new javax.swing.JMenu();
+        jMenu7 = new javax.swing.JMenu();
+        jLabel3 = new javax.swing.JLabel();
+        jMenuBar3 = new javax.swing.JMenuBar();
+        jMenu10 = new javax.swing.JMenu();
+        jMenu8 = new javax.swing.JMenu();
+        jMenu12 = new javax.swing.JMenu();
+
+        jMenu6.setText("File");
+        jMenuBar2.add(jMenu6);
+
+        jMenu7.setText("Edit");
+        jMenuBar2.add(jMenu7);
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Home ");
+        setAutoRequestFocus(false);
+        setBackground(new java.awt.Color(204, 179, 255));
+        setMaximumSize(new java.awt.Dimension(690, 430));
+        setName("frmPrincipal"); // NOI18N
+        setResizable(false);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
+        });
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/home.png"))); // NOI18N
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(-160, -10, 440, 280));
+
+        jMenuBar3.setFocusable(false);
+        jMenuBar3.setName("menuOpcoes"); // NOI18N
+
+        jMenu10.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/map.png"))); // NOI18N
+        jMenu10.setText("Aeroportos");
+        jMenu10.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 12)); // NOI18N
+        jMenu10.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jMenu10MouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jMenu10MouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                jMenu10MouseExited(evt);
+            }
+        });
+        jMenuBar3.add(jMenu10);
+
+        jMenu8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/arrow_branch.png"))); // NOI18N
+        jMenu8.setText("Voos");
+        jMenu8.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 12)); // NOI18N
+        jMenu8.setName(""); // NOI18N
+        jMenu8.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jMenu8MouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jMenu8MouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                jMenu8MouseExited(evt);
+            }
+        });
+        jMenuBar3.add(jMenu8);
+
+        jMenu12.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/door_out.png"))); // NOI18N
+        jMenu12.setText("Sair");
+        jMenu12.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 12)); // NOI18N
+        jMenu12.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jMenu12MouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jMenu12MouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                jMenu12MouseExited(evt);
+            }
+        });
+        jMenuBar3.add(jMenu12);
+
+        setJMenuBar(jMenuBar3);
+        jMenuBar3.getAccessibleContext().setAccessibleName("menuOpcoes");
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenu12MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu12MouseClicked
+        dispose();
+    }//GEN-LAST:event_jMenu12MouseClicked
+    
+    private void jMenu10MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu10MouseExited
+        jMenu10.doClick();
+    }//GEN-LAST:event_jMenu10MouseExited
+
+    private void jMenu12MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu12MouseExited
+        jMenu12.doClick();
+    }//GEN-LAST:event_jMenu12MouseExited
+
+    private void jMenu10MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu10MouseEntered
+        jMenu10.doClick();
+    }//GEN-LAST:event_jMenu10MouseEntered
+
+    private void jMenu12MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu12MouseEntered
+        jMenu12.doClick();
+    }//GEN-LAST:event_jMenu12MouseEntered
+
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+        jMenu12.doClick();
+    }//GEN-LAST:event_formWindowOpened
+
+    private void jMenu10MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu10MouseClicked
+        AeroportosUI desingers = new AeroportosUI(this, true);
+        desingers.setVisible(true);
+    }//GEN-LAST:event_jMenu10MouseClicked
+
+    private void jMenu8MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu8MouseExited
+        jMenu8.doClick();
+    }//GEN-LAST:event_jMenu8MouseExited
+
+    private void jMenu8MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu8MouseEntered
+        jMenu8.doClick();
+    }//GEN-LAST:event_jMenu8MouseEntered
+
+    private void jMenu8MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu8MouseClicked
+        VoosUI voos = new VoosUI(this, true);
+        voos.setVisible(true);
+    }//GEN-LAST:event_jMenu8MouseClicked
+       
+    public static void main(String args[]) 
+    {
+        try 
+        {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) 
+            {
+                if ("Nimbus".equals(info.getName())) 
+                {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } 
+        catch (ClassNotFoundException ex) 
+        {
+            java.util.logging.Logger.getLogger(HomeUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } 
+        catch (InstantiationException ex) 
+        {
+            java.util.logging.Logger.getLogger(HomeUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } 
+        catch (IllegalAccessException ex) 
+        {
+            java.util.logging.Logger.getLogger(HomeUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } 
+        catch (javax.swing.UnsupportedLookAndFeelException ex) 
+        {
+            java.util.logging.Logger.getLogger(HomeUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+    
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater (new Runnable() 
+        {
+            public void run() 
+            {
+                HomeUI home = new HomeUI();
+                home.setVisible(true);          
+            }
+        });      
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JMenu jMenu10;
+    private javax.swing.JMenu jMenu12;
+    private javax.swing.JMenu jMenu6;
+    private javax.swing.JMenu jMenu7;
+    private javax.swing.JMenu jMenu8;
+    private javax.swing.JMenuBar jMenuBar2;
+    private javax.swing.JMenuBar jMenuBar3;
+    // End of variables declaration//GEN-END:variables
+}
+
---
+++ Air-traffic-control/src/interfaces/VoosUI.java
@@ -0,0 +1,147 @@
+package interfaces;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+public class VoosUI extends javax.swing.JDialog {
+
+    public VoosUI(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        initComponents();
+        this.setLocationRelativeTo(null);
+        this.setIconImage(new ImageIcon(getClass().getResource("/imagens/map.png")).getImage());
+        /*jFormattedTextField2.setFormatterFactory(null);
+        jFormattedTextField5.setFormatterFactory(null);
+        jFormattedTextField8.setFormatterFactory(null);
+        jFormattedTextField4.setFormatterFactory(null);*/
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jTabbedPane1 = new javax.swing.JTabbedPane();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jSeparator2 = new javax.swing.JSeparator();
+        jLabel12 = new javax.swing.JLabel();
+        jLabel13 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel15 = new javax.swing.JLabel();
+        jLabel24 = new javax.swing.JLabel();
+        jPanel4 = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle(" Designers");
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel2.setBackground(new java.awt.Color(219, 219, 225));
+        jPanel2.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 11)); // NOI18N
+        jPanel2.addComponentListener(new java.awt.event.ComponentAdapter() {
+            public void componentShown(java.awt.event.ComponentEvent evt) {
+                jPanel2ComponentShown(evt);
+            }
+        });
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel2.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 12)); // NOI18N
+        jLabel2.setText("Código do designer:");
+        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, -1, -1));
+
+        jLabel6.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 12)); // NOI18N
+        jLabel6.setText("Email:");
+        jPanel2.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 140, 40, -1));
+
+        jLabel10.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 12)); // NOI18N
+        jLabel10.setText("Nome:");
+        jLabel10.setToolTipText("");
+        jPanel2.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 80, -1, -1));
+        jPanel2.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 70, 610, 40));
+
+        jLabel12.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 12)); // NOI18N
+        jLabel12.setText("Complemento:");
+        jLabel12.setToolTipText("");
+        jPanel2.add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 260, -1, -1));
+
+        jLabel13.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 12)); // NOI18N
+        jLabel13.setText("Endereço:");
+        jLabel13.setToolTipText("");
+        jPanel2.add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 200, -1, -1));
+
+        jLabel7.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 12)); // NOI18N
+        jLabel7.setText("N°:");
+        jPanel2.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 260, -1, -1));
+
+        jLabel15.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 12)); // NOI18N
+        jLabel15.setText("Telefone:");
+        jPanel2.add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 140, 60, -1));
+
+        jLabel24.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 12)); // NOI18N
+        jLabel24.setText("CEP:");
+        jPanel2.add(jLabel24, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 200, -1, -1));
+
+        jTabbedPane1.addTab("Consultar/ Editar / Excluir  ", jPanel2);
+
+        jPanel4.setBackground(new java.awt.Color(219, 219, 225));
+        jPanel4.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 11)); // NOI18N
+        jPanel4.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel3.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 12)); // NOI18N
+        jLabel3.setText("Código");
+        jPanel4.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 20, -1, -1));
+
+        jTabbedPane1.addTab("Cadastrar  ", jPanel4);
+
+        getContentPane().add(jTabbedPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 30, 610, 370));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/flights.png"))); // NOI18N
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(-320, 0, 1070, 440));
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jPanel2ComponentShown(java.awt.event.ComponentEvent evt) {//GEN-FIRST:event_jPanel2ComponentShown
+
+    }//GEN-LAST:event_jPanel2ComponentShown
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                VoosUI dialog = new VoosUI(new javax.swing.JFrame(), true);
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel15;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel24;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JTabbedPane jTabbedPane1;
+    // End of variables declaration//GEN-END:variables
+
+
+    public static boolean isNumeric(String str) {
+        return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
+    }
+}
