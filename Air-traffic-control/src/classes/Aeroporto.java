e4edabc9b9ab6a8c5b19d7fb3326dd7ddb6f6b0f
Manuela Vicente Benassi <manuelavbenassi@gmail.com>
2020-06-11 14:25:37 -0300

    adição de metodos obrigatórios na classe Aeroporto

--- Air-traffic-control/src/classes/Aeroporto.java
+++ Air-traffic-control/src/classes/Aeroporto.java
@@ -21,7 +21,16 @@ public class Aeroporto implements Cloneable
     private String pais = "";
     private String cidade = "";
     private final ListaDuplamenteLigadaDesordenada<Voo> listaVoos;
-    
+
+    /**
+     * Construtor da classe Aeroporto
+     * 
+     * @param codigoIATA
+     * @param codigoICAO
+     * @param nome
+     * @param pais
+     * @param cidade
+     */
     public Aeroporto(String codigoIATA, String codigoICAO, String nome, String pais, String cidade)
     {
         this.codigoIATA = codigoIATA;
@@ -102,5 +111,57 @@ public class Aeroporto implements Cloneable
         else
         this.cidade = cidade;
     }
+    public String toString(){
+        return "IdAeroporto: " + this.idAeroporto + "" + " codigo IATA: " + codigoIATA + " codigo ICAO " + codigoICAO +
+        " nome areporto " + nomeAero + " pais: " + pais + " cidade " + cidade;
+
+    }
+    public int haschCode(){
+        int ret = 111;
+        ret = ret *5 + this.cidade.hashCode();
+        ret = ret * 5 + this.codigoIATA.hashCode();
+        ret = ret * 5 + this.codigoICAO.hashCode();
+        ret = ret * 5 + new Integer(this.idAeroporto).haschCode();
+        ret = ret * 5 + this.nomeAero.hashCode();
+        ret = ret * 5 + this.pais.hashCode();
+        if(ret<0)
+            ret = -ret;
+
+        return ret;
+    }
+    public Object clone(){
+        Aeroporto clone = null;
+        try{
+            Aeroporto = new Aeroporto(this);
+        }
+        catch(Exception e){}
+        return clone;
+
+    }
+    public boolean equals(Object obj){
+        if(obj == null)
+            return false;
+        if(obj == this)
+            return true;
+        if(obj.getClass() != this.getClass())
+            return false;
+        Aeroporto aero = (Aeroporto) obj;
+        if(!this.cidade.equals(aero.cidade) || !this.pais.equals(aero.pais) || !this.codigoIATA.equals(aero.codigoIATA) ||
+       !this.codigoICAO.equals(aero.codigoICAO) || this.idAeroporto != aero.idAeroporto || !this.nomeAero.equals(aero.nomeAero))
+            return false;
+        return true;
+    }
+    public Aeroporto(Aeroporto modelo){
+        if(modelo == null){
+            throw new Exception("modelo para cópia não pode ser nulo");
+        }
+        this.cidade = modelo.cidade;
+        this.codigoIATA = modelo.codigoIATA;
+        this.codigoICAO = modelo.codigoICAO;
+        this.idAeroporto = modelo.idAeroporto;
+        this.nomeAero = modelo.nomeAero;
+        this.pais = modelo.pais;
+    }
+
     
 }
